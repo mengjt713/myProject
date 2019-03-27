4714cf98ac31ad45e4473288bc9bfffe2a73beeb
mengjintao <mengjt713@163.com>
2019-03-28 01:45:38 +0800

    修改样式，调整

--- client/module/module-agent/agentController.js
+++ client/module/module-agent/agentController.js
@@ -1,15 +1,26 @@
 define(["app"], function(myApp) {
     myApp.register.controller('agentController', function ($scope, $location, $state, $rootScope, msgService, httpService) {
-        httpService.post({
-            url: 'action/agent/getAgent',
-            data:{
-                number:'1'
-            },
-            onSuccess: function(response){
-            },
-            onError: function(response){
-                msgService.promt(response.message);
-            }
-        });
+        $scope.curSelectTab = "all";
+        $scope.queryOs = function(type){
+            $scope.curSelectTab = type;
+        };
+        function getOs(type){
+            httpService.post({
+                url: 'action/agent/getAgent',
+                data:{
+                    type:type
+                },
+                onSuccess: function(response){
+                    if(response){
+                        $scope.osList = response;
+                    }
+                },
+                onError: function(response){
+                    msgService.promt(response.message);
+                }
+            });
+        }
+        getOs('all');
+
     })
 })
